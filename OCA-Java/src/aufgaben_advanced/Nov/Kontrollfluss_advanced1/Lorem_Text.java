813e18ab52d595f2c7f7e9f85196bae671a08dc0
Darwin Paz <142513909+dwn10@users.noreply.github.com>
2023-12-05 23:13:38 +0100

    05.12.2023

---
+++ OCA-Java/src/aufgaben_advanced/Nov/Kontrollfluss_advanced1/Lorem_Text.java
@@ -0,0 +1,62 @@
+package aufgaben_advanced.Nov.Kontrollfluss_advanced1;
+
+import java.util.Scanner;
+
+public class Lorem_Text {
+
+    // Hauptmethode
+    public static void main(String[] args) {
+
+        // Scanner erstellen
+        Scanner scanner = new Scanner(System.in);
+
+        // Benutzer nach einem Text fragen
+        System.out.println("\n*************************");
+        System.out.println("Geben Sie einen Text ein: \n");
+        String text = scanner.nextLine();
+
+        // Wörter in Großbuchstaben umwandeln
+        text = text.toUpperCase();
+
+        // Array mit Sonderzeichen erstellen
+        String[] lorem = { "lorem", "LOREM", "LOREM.", "LOREM!", "LOREM?", "-LOREM", "1.LOREM", "-LOREM", "*LOREM" };
+
+        // Wörter zählen
+        int wortzaehlung = 0;
+        for (String wort : text.split(" ")) {
+            if (istLoremWort(wort, lorem)) {
+                wortzaehlung++;
+            } else {
+                wortzaehlung++;
+            }
+        }
+
+        // Wörter ausgeben
+        System.out.println("*************************");
+        System.out.println("Die Gesamtzahl der Wörter beträgt: " + wortzaehlung);
+        System.out.println("\n*************************");
+        System.out.println("Die Anzahl der Wörter, die " + " enthalten, beträgt: "
+                + zaehleLoremWoerter(text, lorem));
+    }
+
+    // Methode, um zu überprüfen, ob ein Wort ein Lorem-Wort ist
+    private static boolean istLoremWort(String wort, String[] lorem) {
+        for (String s : lorem) {
+            if (wort.equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Methode, um die Anzahl der Lorem-Wörter in einem Text zu zählen
+    private static int zaehleLoremWoerter(String text, String[] lorem) {
+        int zaehlung = 0;
+        for (String wort : text.split(" ")) {
+            if (istLoremWort(wort, lorem)) {
+                zaehlung++;
+            }
+        }
+        return zaehlung;
+    }
+}
