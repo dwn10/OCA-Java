bdac30f992abfa4748a7774f775f15ed8885c5de
Darwin Paz <paz.darwin1080@gmail.com>
2024-01-23 15:08:58 +0100

    23

--- OCA-Java/src/aufgaben_advanced/Nov/Kontrollfluss_advanced1/BrailleCodeUebersetzen.java
+++ OCA-Java/src/aufgaben_advanced/Nov/Kontrollfluss_advanced1/BrailleCodeUebersetzen.java
@@ -11,12 +11,46 @@ public class BrailleCodeUebersetzen {
         Scanner scanner = new Scanner(System.in);
         // BrailleCode representation for letters
         String[] brailleABC = {
-                "⠁", "⠃", "⠉ ", "⠙ ", "⠑ ", "⠋ ", "⠛ ", "⠓ ", "⠊ ", "⠚ ", "⠅ ", "⠇ ", "⠍ ", "⠝", "⠕",
-                "⠏ ", "⠟ ", "⠗ ", "⠎ ", "⠞", "⠥", "⠧", "⠺ ", "⠭ ", "⠽ ", "⠵"
+                "⠁", // a
+                "⠃", // b
+                "⠉ ", // c
+                "⠙ ", // d
+                "⠑ ", // e
+                "⠋ ", // f
+                "⠛ ", // g
+                "⠓ ", // h
+                "⠊ ", // i
+                "⠚ ", // j
+                "⠅ ", // k
+                "⠇ ", // l
+                "⠍ ", // m
+                "⠝", // n
+                "⠕", // o
+                "⠏ ", // p
+                "⠟ ", // q
+                "⠗ ", // r
+                "⠎ ", // s
+                "⠞", // t
+                "⠥", // u
+                "⠧", // v
+                "⠺ ", // w
+                "⠭ ", // x
+                "⠽ ", // y
+                "⠵" // z
         };
 
-        String[] nummer_0_9 = { "⠼⠚ ", "⠼⠁ ", "⠼⠃ ", "⠼⠉ ", "⠼⠙ ", "⠼⠑ ", "⠼⠋ ", "⠼⠛ ", "⠼⠓ ",
-                "⠼⠊ " };
+        String[] nummer_0_9 = {
+                "⠼⠚ ", // 0
+                "⠼⠁ ", // 1
+                "⠼⠃ ", // 2
+                "⠼⠉ ", // 3
+                "⠼⠙ ", // 4
+                "⠼⠑ ", // 5
+                "⠼⠋ ", // 6
+                "⠼⠛ ", // 7
+                "⠼⠓ ", // 8
+                "⠼⠊ " // 9
+        };
 
         System.out.println("\n******************************************************************");
         System.out.println("Geben Sie den Text ein, den Sie in BrailleCode übersetzen möchten:");
